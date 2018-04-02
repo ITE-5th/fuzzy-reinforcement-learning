07dda690d23072835b33d2537dabd6407ab99d9f
Obada Jabassini <obada.jabassini@gmail.com>
2018-04-02 23:16:37 +0300

    Add initial states

--- train.py
+++ train.py
@@ -11,14 +11,16 @@ np.seterr(divide='ignore', invalid='ignore')
 gym.logger.set_level(40)
 number_of_rules = 2
 discount_factor = 0.97
-number_of_episodes = 100
+number_of_episodes = 1000
 horizon = 200
 env: TimeLimit = gym.make('CartPole-v0')
 dimension = env.observation_space.shape[0]
+initial_states = np.random.uniform(-0.5, 0.5, (number_of_episodes, dimension))
+initial_states[:, 1] = initial_states[:, 3] = np.zeros(number_of_episodes)
 
 
-def run_episode(fuzzy_system: FuzzySystem):
-    observation = env.reset()
+def run_episode(observation, fuzzy_system: FuzzySystem):
+    env.reset()
     total_reward = 0
     for i in range(horizon):
         action = fuzzy_system.take_action(observation)
@@ -33,8 +35,8 @@ def run_episode(fuzzy_system: FuzzySystem):
 def particle_reward(params):
     fuzzy_system = FuzzySystem(params, number_of_rules, dimension)
     total_reward = 0
-    for _ in range(number_of_episodes):
-        total_reward += run_episode(fuzzy_system)
+    for i in range(number_of_episodes):
+        total_reward += run_episode(initial_states[i], fuzzy_system)
     total_reward /= number_of_episodes
     return total_reward
 
@@ -46,6 +48,7 @@ def fitness(X):
 options = {'c1': 2.05, 'c2': 2.05, 'w': 0.729, "k": 30, 'p': 2}
 optimizer = ps.single.LocalBestPSO(n_particles=100, dimensions=(2 * dimension + 1) * number_of_rules + 1,
                                    options=options)
+print("begin training")
 cost, pos = optimizer.optimize(fitness, print_step=1, iters=1000, verbose=3)
 with open("result.pkl", "wb") as f:
     pickle.dump({"cost": cost, "best": pos, "dimension": dimension, "number_of_rules": number_of_rules}, f)
